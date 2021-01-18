5d07adda89a1905ae82246b3c84df26e16dea320
girkarkshitij <girkarkshitij@gmail.com>
2021-01-18 12:23:19 +0530

    Add UserItem.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends React.Component {
   render() {
-    return <Navbar />;
+    return (
+      <div className='App'>
+        <Navbar />
+        <UserItem />
+      </div>
+    );
   }
 }
 
