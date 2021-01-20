feaf3f59a3e50493e90d122ead9061abc4fb4584
girkarkshitij <girkarkshitij@gmail.com>
2021-01-20 12:36:00 +0530

    Add Search.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -10,19 +11,20 @@ class App extends React.Component {
     loading: false,
   };
 
-  async componentDidMount() {
+  searchUsers = async (text) => {
     this.setState({ loading: true });
     const result = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
-    this.setState({ users: result.data, loading: false });
-  }
+    this.setState({ users: result.data.items, loading: false });
+  };
 
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Search searchUsers={this.searchUsers} />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
