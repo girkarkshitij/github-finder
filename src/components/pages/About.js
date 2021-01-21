fb5e415c1e6c5aa192f2cd84e665fbcc8dc21973
girkarkshitij <girkarkshitij@gmail.com>
2021-01-21 12:04:16 +0530

    Install React Router & create About.js

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About</h1>
+      <p>App to search github users</p>
+      <p>Made by Kshitij</p>
+    </Fragment>
+  );
+};
+
+export default About;
